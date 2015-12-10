25da82d8b39d471837ef0e22936ae38d0b341ba8
Brandon Kindred <brandon.kindred@gmail.com>
2015-12-10 01:03:02 -0500

    implemented slide tabs and added in barcode scanner.

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,11 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('Buzzbands_mobile', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('buzzbands_mobile', ['ionic',
+                                    'starter.controllers',
+                                    'starter.services',
+                                    'ngCordova',
+                                    'tabSlideBox'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -32,54 +36,54 @@ angular.module('Buzzbands_mobile', ['ionic', 'starter.controllers', 'starter.ser
   $stateProvider
 
   // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
+  .state('tab', {
+    url: '/',
     abstract: true,
-    templateUrl: 'templates/tabs.html'
+    templateUrl: './templates/tabs.html'
   })
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.scan', {
+    url: '/scan',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'tab-scan': {
+        templateUrl: './templates/scan/index.html',
+        controller: 'ScanCtrl'
       }
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.venues', {
+      url: '/venues',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+        'tab-venues': {
+          templateUrl: 'templates/venues/index.html',
+          controller: 'VenueIndexCtrl'
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
+    .state('tab.promotions', {
+      url: '/promotions/:promotionId',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        'tab-promotions': {
+          templateUrl: 'templates/promotions/index.html',
+          controller: 'PromotionIndexCtrl'
         }
       }
     })
 
-  .state('tab.account', {
-    url: '/account',
+  .state('tab.settings', {
+    url: '/settings',
     views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+      'tab-settings': {
+        templateUrl: 'templates/settings/index.html',
+        controller: 'SettingCtrl'
       }
     }
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/');
 
 });
