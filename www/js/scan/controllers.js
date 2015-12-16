e278e267377294ccfd151e5e5197cfca9c4b7d59
Brandon Kindred <brandon.kindred@gmail.com>
2015-12-15 21:06:55 -0500

    moved from a slider setup to a title bar with a button for scanning

--- www/js/scan/controllers.js
+++ www/js/scan/controllers.js
@@ -1,22 +1,23 @@
 angular.module('scan.controllers', [])
-
+.controller('ScanAccessCtrl', function($scope, $cordovaBarcodeScanner, $ionicPlatform, $state) {
+  $scope.openScanner = function(){
+    $state.go('scan');
+  }
+})
 .controller('ScanCtrl', function($scope, $cordovaBarcodeScanner, $ionicPlatform, $state) {
   $scope.$on('$ionicView.enter', function() {
     document.addEventListener("deviceready", function () {
-
-      $scope.status = "scanning";
 
       $cordovaBarcodeScanner
         .scan()
         .then(function(result) {
             // Success! Barcode data is here
             $scope.scanResults = "We got a barcoden" +
-            "Result: " + result.text + "n" +
-            "Format: " + result.format + "n" +
+            "Result: " + result.text + "\n" +
+            "Format: " + result.format + "\n" +
             "Cancelled: " + result.cancelled;
 
-            //alert(result.text);
-            $scope.result = result.text;
+            alert(result.text);
             $state.go( 'venuePromotions', result.text );
         }, function(error) {
             // An error occurred
@@ -24,9 +25,6 @@ angular.module('scan.controllers', [])
         });
     });
   })
-
-  $state.go( 'venuePromotions', 12345 );
-
 
   $scope.scanResults = '';
 })
