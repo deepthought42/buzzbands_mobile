941b52a5dcb0b52a02ab154020f0b57a8ee57db1
Brandon Kindred <brandon.kindred@gmail.com>
2016-01-19 20:23:11 -0500

    changed source to staging server in serviceConfig.js

--- www/js/app.js
+++ www/js/app.js
@@ -23,7 +23,6 @@ angular.module('buzzbands_mobile', ['ionic',
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
